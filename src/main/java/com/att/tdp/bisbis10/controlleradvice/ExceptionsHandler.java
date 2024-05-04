40dc9e3643704b78c7b1a4dc5c219709964fadc1
jaafari-R <jaafariramadan@gmail.com>
2024-05-04 17:11:30 +0300

    Initialized ExceptionHandler and created ErrorResponse class

---
+++ src/main/java/com/att/tdp/bisbis10/controlleradvice/ExceptionsHandler.java
@@ -0,0 +1,8 @@
+package com.att.tdp.bisbis10.controlleradvice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+    
+}
